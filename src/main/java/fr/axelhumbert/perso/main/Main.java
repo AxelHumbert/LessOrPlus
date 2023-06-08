88971960fbec7f6540f188337fac723d4db7e5cb
AxelHumbert <axelhumbert5@gmail.com>
2023-06-08 15:22:04 +0200

    ADD: Start project

---
+++ src/main/java/fr/axelhumbert/perso/main/Main.java
@@ -0,0 +1,37 @@
+package fr.axelhumbert.perso.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+  private static void usage() {
+    System.out.println("USAGE : java -jar LessOrPlus.jar maxNumber");
+  }
+
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      usage();
+      return;
+    }
+
+    var max = Integer.parseInt(args[0]);
+    var min = 1;
+    var researchedNumber = Math.random() * (max - min);
+    var scanner = new Scanner(System.in);
+
+    System.out.println("Give me a number between " + min + " and " + max + " : ");
+    var givenNumber = scanner.nextInt();
+
+    while (givenNumber != researchedNumber) {
+      if (givenNumber < researchedNumber) {
+        System.out.println("It's plus !");
+      } else {
+        System.out.println("It's less !");
+      }
+      givenNumber = scanner.nextInt();
+    }
+
+    System.out.println("Congratulations ! You win and the number to search was " + researchedNumber);
+    scanner.close();
+  }
+}
